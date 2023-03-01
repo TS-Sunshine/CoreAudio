f4e8020c0e03f1380f0d234fb6b984fa6a5e9776
马彪 <biao.ma@aispeech.com>
2023-03-01 19:17:33 +0800

    feat:CAMateData

---
+++ AICoreAudio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AICoreAudio
+//
+//  Created by 三虎 on 2023/3/1.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
